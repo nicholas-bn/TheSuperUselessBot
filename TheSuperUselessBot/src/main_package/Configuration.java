7f806e2176880de52e52655718665675fe374e3f
Barnini Nicholas <nicholas.barnini@gmail.com>
2016-08-15 20:36:42 +0200

    #2 JSON parser pour créer une ArrayList<Commande>

--- TheSuperUselessBot/src/main_package/Configuration.java
+++ TheSuperUselessBot/src/main_package/Configuration.java
@@ -1,10 +1,16 @@
 package main_package;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.ini4j.InvalidFileFormatException;
 import org.ini4j.Wini;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 
 public class Configuration {
 
@@ -87,6 +93,51 @@ public class Configuration {
 			System.out.println("IOException");
 			e.printStackTrace();
 		}
+	}
+	
+	public ArrayList<Commande> setupCommands(){
+	ArrayList<Commande> returnCommand = new ArrayList<Commande>();
+		
+		JSONParser parser = new JSONParser();
+
+        try {
+            FileReader fileReader = new FileReader("d:\\TRAVAIL\\Perso\\Jar_BOT\\command_list.json");
+            JSONObject json = (JSONObject) parser.parse(fileReader);
+            JSONArray commands = (JSONArray) json.get("commands");
+            
+            for(int j=0; j<commands.size();j++){
+            	JSONObject tempoCommande = (JSONObject) commands.get(j);
+            	
+            	String nom = tempoCommande.get("nomCommande").toString();
+            	String resultat = tempoCommande.get("resultatCommande").toString();
+            	boolean activated = Boolean.parseBoolean(tempoCommande.get("activated").toString());
+            	boolean regexp = Boolean.parseBoolean(tempoCommande.get("isRegExp").toString());
+            	
+//            	if(nom == null){
+//            		System.out.println("ERREUR : nomCommande("+j+") == null dans e JSON");
+//            	}
+//            	if(resultat == null){
+//            		System.out.println("ERREUR : nomCommande("+j+") == null dans e JSON");
+//            	}
+//            	if(activated == null){
+//            		System.out.println("ERREUR : nomCommande("+j+") == null dans e JSON");
+//            	}
+//            	if(regexp == null){
+//            		System.out.println("ERREUR : nomCommande("+j+") == null dans e JSON");
+//            	}
+            	
+            	Commande c = new Commande(nom, resultat, activated, regexp);
+            	System.out.println(c.getNomCommande()+c.getResultatCommande()+c.isActivated()+c.isRegExp());
+            	returnCommand.add(c);
+            	
+            }
+            
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+		
+		return returnCommand;
 	}
 	
 }
