d01f976855abd40152c6216fd13a894fc13ba05e
Barnini Nicholas <nicholas.barnini@gmail.com>
2016-08-17 18:52:52 +0200

    #3 Création du premier JUNIT pour Commande

---
+++ TheSuperUselessBot/test/main_package_test/CommandeTest.java
@@ -0,0 +1,27 @@
+package main_package_test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import main_package.Commande;
+import main_package.TwitchBot;
+
+public class CommandeTest {
+
+	TwitchBot t;
+	
+	
+	@Before
+	public void initialize() {
+		
+	}
+	
+	@Test
+	public void testCheckCommand() {
+		fail("Not yet implemented");
+		Commande c;
+	}
+
+}
