efd9ed2075daf0e4d247ca944a17121e2be61676
Barnini Nicholas <nicholas.barnini@gmail.com>
2016-08-26 15:41:02 +0200

    #8 Ajout de menu dans la JFrame

--- TheSuperUselessBot/src/GUI_package/WindowLauncher.java
+++ TheSuperUselessBot/src/GUI_package/WindowLauncher.java
@@ -1,5 +1,7 @@
 package GUI_package;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
@@ -7,35 +9,80 @@ import java.awt.event.WindowListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 
 public class WindowLauncher  extends JFrame { 
 
-	   public WindowLauncher() {
-		     super("titre de l'application");
-
-		      WindowListener l = new WindowAdapter() {
-		         public void windowClosing(WindowEvent e){
-		            System.exit(0);
-		         }
-		      };
-		      
-		      System.out.println("test");
-
-		      addWindowListener(l);
-
-		      ImageIcon img = new ImageIcon("tips.gif");
-		      JButton bouton = new JButton("Mon bouton",img);
-
-		      JPanel panneau = new JPanel();
-		      panneau.add(bouton);
-		      setContentPane(panneau);
-		      setSize(200,100);
-		      setVisible(true);
-		   }
-
-	   public static void main(String [] args){
-		   WindowLauncher frame = new WindowLauncher();
-	   } 
+	public WindowLauncher() {
+		super("TheSuperUselessBot");
+		
+		WindowListener l = new WindowAdapter() {
+			public void windowClosing(WindowEvent e){
+				System.exit(0);
+			}
+		};
+		
+		JMenuBar menuBar = new JMenuBar();
+		JMenu commandesMenu = new JMenu("Commandes");
+		JMenu jeuxMenu = new JMenu("Jeux");
+		JMenu outilsMenu = new JMenu("Outils");
+		
+		commandesMenu.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("HUEHEUHEU");
+				
+			}
+		});
+		
+		menuBar.add(commandesMenu);
+		menuBar.add(jeuxMenu);
+		menuBar.add(outilsMenu);
+		this.setJMenuBar(menuBar);
+		  
+		System.out.println("test");
+		
+		addWindowListener(l);
+		
+		ImageIcon img = new ImageIcon("tips.gif");
+		JButton bouton = new JButton("Mon bouton",img);
+		
+		JPanel panneau = new JPanel();
+		panneau.add(bouton);
+		setContentPane(panneau);
+		setSize(200,100);
+		setVisible(true);
+	}
+	
+	public static void main(String [] args){
+		WindowLauncher frame = new WindowLauncher();
+	} 
 
 }
