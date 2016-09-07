b45375d14c1ee854bfa9609e3ae21c26b13dd5df
Barnini Nicholas <nicholas.barnini@gmail.com>
2016-09-07 14:47:04 +0200

    #10 Ajout de DocumentListener pour les JTextArea et ItemListener pour les JCheckBox

--- TheSuperUselessBot/src/GUI_package/Commande_JPanel.java
+++ TheSuperUselessBot/src/GUI_package/Commande_JPanel.java
@@ -1,9 +1,19 @@
 package GUI_package;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
 
 import main_package.Commande;
 
@@ -25,13 +35,130 @@ public class Commande_JPanel extends JPanel {
 		activated = new JCheckBox("Actif", c.isActivated());
 		modOnly = new JCheckBox("modOnly", c.isModOnly());
 		isRegExp = new JCheckBox("isRegExp", c.isRegExp());
+		commande = c;
 	    //this.setLayout(new BoxLayout(this,BoxLayout.X_AXIS));
+		
+		this.addListeners();
+		
 		this.add(activated);
 		this.add(nomCommande);
 		this.add(resultatCommande);
 		this.add(modOnly);
 		this.add(isRegExp);
 		
+	}
+	
+	public void addListeners(){
+		//ACTIVATED
+		activated.addItemListener(new ItemListener() {
+			
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				// TODO Auto-generated method stub
+				if(e.getStateChange()==ItemEvent.SELECTED){
+					commande.setActivated(true);
+				}else if (e.getStateChange()==ItemEvent.DESELECTED){
+					commande.setActivated(false);
+				}
+				System.out.println(Commande_JPanel.this.getCommande().toString());
+			}
+		});
+		
+		//MODONLY
+		modOnly.addItemListener(new ItemListener() {
+			
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				// TODO Auto-generated method stub
+				if(e.getStateChange()==ItemEvent.SELECTED){
+					commande.setModOnly(true);
+				}else if (e.getStateChange()==ItemEvent.DESELECTED){
+					commande.setModOnly(false);
+				}
+				System.out.println(Commande_JPanel.this.getCommande().toString());
+			}
+		});
+		
+		//REGEXP
+		isRegExp.addItemListener(new ItemListener() {
+			
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				// TODO Auto-generated method stub
+				if(e.getStateChange()==ItemEvent.SELECTED){
+					commande.setRegExp(true);
+				}else if (e.getStateChange()==ItemEvent.DESELECTED){
+					commande.setRegExp(false);
+				}
+				System.out.println(Commande_JPanel.this.getCommande().toString());
+			}
+		});
+		
+		// NOM COMMANDE
+		DocumentListener documentListenerNom = new DocumentListener() {
+			
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("removeUpdate nomCommande");
+				textChanged(e);
+			}
+
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("insertUpdate nomCommande");
+				textChanged(e);
+				
+			}
+			
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("changedUpdate nomCommande");
+				
+			}
+			
+			private void textChanged(DocumentEvent e) {
+				commande.setNomCommande(nomCommande.getText());
+				System.out.println(Commande_JPanel.this.getCommande().toString());				
+			}
+			
+		};
+		nomCommande.getDocument().addDocumentListener(documentListenerNom);
+		
+		// RESULTAT COMMANDE
+		DocumentListener documentListenerResultat = new DocumentListener() {
+			
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("removeUpdate resultatCommande");
+				textChanged(e);
+			}
+
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("insertUpdate resultatCommande");
+				textChanged(e);
+				
+			}
+			
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("changedUpdate resultatCommande");
+				
+			}
+			
+			private void textChanged(DocumentEvent e) {
+				commande.setResultatCommande(resultatCommande.getText());
+				System.out.println(Commande_JPanel.this.getCommande().toString());				
+			}
+			
+		};
+		resultatCommande.getDocument().addDocumentListener(documentListenerResultat);
 	}
 
 
