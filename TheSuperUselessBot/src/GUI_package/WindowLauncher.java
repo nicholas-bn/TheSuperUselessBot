35223d32a7e83c2a059ee8c34562ea3eb4810187
Barnini Nicholas <nicholas.barnini@gmail.com>
2016-09-09 14:31:01 +0200

    #10 Ajout d'un bouton pour ajouter une commande

--- TheSuperUselessBot/src/GUI_package/WindowLauncher.java
+++ TheSuperUselessBot/src/GUI_package/WindowLauncher.java
@@ -1,6 +1,8 @@
 package GUI_package;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
@@ -8,8 +10,9 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
@@ -37,13 +40,17 @@ public class WindowLauncher  extends JFrame {
 			}
 		};
 		
+		
 		this.addWindowListener(l);
+		
 		
 		remplirMenu();
 		loadConfig();
 
+		
 		parent = new JPanel();
 	    parent.setLayout(new BoxLayout(parent,BoxLayout.PAGE_AXIS));
+	    
 	    
 	    // Permet d'avoir une list des JPanel pour pouvoir les remttre en list de Commande à donner au bot
 	    for(Commande c : config.getListCommandes()){
@@ -52,24 +59,49 @@ public class WindowLauncher  extends JFrame {
 	    for(Commande_JPanel cjp : listPanelCommande){
 	    	parent.add(cjp);
 	    }
+	    
+	    
 	   	// Ce JPanel permet d'éviter l'espacement entre les Commande_JPanel 
 	   	JPanel mainPanel = new JPanel(new BorderLayout());
 		mainPanel.add(parent, BorderLayout.PAGE_START);
 		
+		
+		JButton ajoutCommande = new JButton("Ajouter une commande", new ImageIcon("ressources/images/Bouton_Ajout.png"));
+		ajoutCommande.setToolTipText("Permet d'ajouter une commande");
+		ajoutCommande.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				Commande c = new Commande("nomCommande", "resultatCommande", false, false, false);
+				Commande_JPanel cjp  = new Commande_JPanel(c, WindowLauncher.this);
+				WindowLauncher.this.listPanelCommande.add(cjp);
+				WindowLauncher.this.parent.add(cjp);
+				//WindowLauncher.this.updateListCommand();		
+				WindowLauncher.this.invalidate();
+				WindowLauncher.this.validate();
+				WindowLauncher.this.repaint();
+			}
+		});
+		mainPanel.add(ajoutCommande, BorderLayout.PAGE_END);
+		
+		
 		this.add(mainPanel);  
-			   	
+			  
+		
 		//this.setSize(960,1080);
 		this.setSize(1060,1080);
 		this.setLocation(1920, 0);
 		this.setVisible(true);
 		
 
-		
+		// UTILISER CA POUR SWAP ENTRE COMMANDE OUTILS MENUS
+		//mainPanel.setVisible(false);
 
-		System.out.println("THREAD window: "+Thread.currentThread().getId());
-		tb = new TwitchBot(config);
-		Thread t = new Thread(tb);
-		t.start();
+//		System.out.println("THREAD window: "+Thread.currentThread().getId());
+//		tb = new TwitchBot(config);
+//		Thread t = new Thread(tb);
+//		t.start();
 		
 
 		
--- TheSuperUselessBot/src/main_package/TwitchBot.java
+++ TheSuperUselessBot/src/main_package/TwitchBot.java
@@ -1,7 +1,6 @@
 package main_package;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
