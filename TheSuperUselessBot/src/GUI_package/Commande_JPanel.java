b21e7828463994401685d116798735123ab9d638
Barnini Nicholas <nicholas.barnini@gmail.com>
2016-09-13 14:09:18 +0200

    #15 Résolution du bug lors d'espace avant/aprés la commande

--- TheSuperUselessBot/src/GUI_package/Commande_JPanel.java
+++ TheSuperUselessBot/src/GUI_package/Commande_JPanel.java
@@ -4,17 +4,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
@@ -30,6 +27,7 @@ public class Commande_JPanel extends JPanel {
 	private JButton supprimer;
 	private Commande commande;
 	private WindowLauncher anchor;
+	public String tempoRoot = "";
 	
 	
 
@@ -140,7 +138,7 @@ public class Commande_JPanel extends JPanel {
 			}
 			
 			private void textChanged(DocumentEvent e) {
-				commande.setNomCommande(nomCommande.getText());
+				commande.setNomCommande(nomCommande.getText().trim());
 				System.out.println(Commande_JPanel.this.getCommande().toString());
 				anchor.updateListCommand();				
 			}
